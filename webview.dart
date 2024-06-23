ab41203bbb177833a1ba7c19c4c796d6f073d423
MRikoKuswoyo <muhammadrikokuswoyo5104@gmail.com>
2024-06-23 11:48:28 +0700

    Add files via upload

---
+++ webview.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Webview extends StatefulWidget {
+  const Webview({super.key});
+
+  @override
+  State<Webview> createState() => _WebviewState();
+}
+
+class _WebviewState extends State<Webview> {
+  late WebViewController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('http://192.168.43.87:8000')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('http://192.168.43.87:8000'));
+
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
+      ),
+    );
+  }
+}
